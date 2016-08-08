94880fb6ee18ebc792a7c55c6f4f82d2d66573c9
Trevor Preston <trevorpreston4@gmail.com>
2016-08-08 12:42:09 -0400

    added gpio tasks to buttons

--- models/bot.js
+++ models/bot.js
@@ -1,41 +1,41 @@
-// var gpio = require('rpi-gpio');
-
-
-// function turnOn(req,res,next){
-//   gpio.setMode(MODE_BCM);
-//   gpio.setup(7, gpio.DIR_OUT, write);
-
-//   function write(){
-//     gpio.write(7, true, function(err){
-//       if(err) throw err;
-//       console.log('pump is on!')
-//     })
-//   }
-// }
-
-
-// function turnOff(req,res,next){
-//   gpio.setMode(MODE_BCM);
-//   gpio.setup(7, gpio.DIR_OUT, write);
-
-//   function write(){
-//     gpio.write(7, false, function(err){
-//       if(err) throw err;
-//       console.log('pump turned off!')
-//     })
-//   }
-// }
+var gpio = require('rpi-gpio');
 
 
 function turnOn(req,res,next){
-  console.log("ROBOT ON!!!!!!");
-  next()
+  gpio.setMode(MODE_BCM);
+  gpio.setup(7, gpio.DIR_OUT, write);
+
+  function write(){
+    gpio.write(7, true, function(err){
+      if(err) throw err;
+      console.log('pump is on!')
+    })
+  }
 }
 
 
 function turnOff(req,res,next){
-  console.log("ROBOT OFF!!");
-  next()
+  gpio.setMode(MODE_BCM);
+  gpio.setup(7, gpio.DIR_OUT, write);
+
+  function write(){
+    gpio.write(7, false, function(err){
+      if(err) throw err;
+      console.log('pump turned off!')
+    })
+  }
 }
+
+
+// function turnOn(req,res,next){
+//   console.log("ROBOT ON!!!!!!");
+//   next()
+// }
+
+
+// function turnOff(req,res,next){
+//   console.log("ROBOT OFF!!");
+//   next()
+// }
 
 module.exports = { turnOn, turnOff }
