a584769d64b39fa06990fb26144f4b63638b41df
Trevor Preston <trevorpreston4@gmail.com>
2016-08-11 11:40:55 -0400

    stashing, don't merge

--- models/bot.js
+++ models/bot.js
@@ -35,6 +35,32 @@ cron.schedule( cycleTime, function(){
 // }
 
 
+// function turnOn(req,res,next){
+//   gpio.setup(7, gpio.DIR_OUT, write);
+
+//   function write(){
+//     gpio.write(7, true, function(err){
+//       if(err) throw err;
+//       console.log('pump is on!')
+//     })
+//   }
+//   next()
+// }
+
+
+// function turnOff(req,res,next){
+//   gpio.setup(7, gpio.DIR_OUT, write);
+
+//   function write(){
+//     gpio.write(7, false, function(err){
+//       if(err) throw err;
+//       console.log('pump turned off!')
+//     })
+//   }
+//   next()
+// }
+
+
 // // function turnOn(req,res,next){
 // //   console.log("ROBOT ON!!!!!!");
 // //   next()
