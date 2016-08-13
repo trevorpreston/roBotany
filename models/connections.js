d517714a8c7652e85f7969e8504c071aeea9e2e0
Trevor Preston <trevorpreston4@gmail.com>
2016-08-13 19:56:52 -0400

    ud

--- models/connections.js
+++ models/connections.js
@@ -1,11 +1,11 @@
 const pg = require('pg-promise')({});
 
 const config = {
-  host:       process.env.DB_HOST,
-  port:       process.env.DB_PORT,
+  host:       'localhost',
+  port:       '5432',
   database:   'robotanist',
-  user:       postgres,
-  password:   vaultin10,
+  user:       'postgres',
+  password:   'vaultin10'
 };
 const db = pg(config);
 
